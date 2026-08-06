7d10bb9d9a08ce363a0be8cff2716103f0a326fe
li <li@goldragon.criome.net>
2026-08-06 17:52:53 +0200

    triad-runtime: converge on the sole Frame world

--- tests/signal_frame_boundary.rs
+++ tests/signal_frame_boundary.rs
@@ -1,11 +1,19 @@
 #[test]
-fn signal_frame_dependency_is_one_exact_zero_point_four_producer() {
+fn signal_frame_dependency_is_the_sole_exact_producer() {
     let manifest = include_str!("../Cargo.toml");
-    let expected = "signal-frame = { git = \"https://github.com/LiGoldragon/signal-frame.git\", rev = \"0786fbe8caf27552afcdd5deb85bc82ec6088337\" }";
+    let lockfile = include_str!("../Cargo.lock");
+    let expected = "signal-frame = { git = \"https://github.com/LiGoldragon/signal-frame.git\", rev = \"8aa0bcaeb29fe9e461a11706a469638d2fd109ac\", default-features = false }";
 
     assert_eq!(manifest.matches("signal-frame =").count(), 1);
     assert!(manifest.contains(expected));
-    assert!(!manifest.contains("signal-frame.git\", branch"));
+    assert!(lockfile.contains(
+        "signal-frame.git?rev=8aa0bcaeb29fe9e461a11706a469638d2fd109ac#8aa0bcaeb29fe9e461a11706a469638d2fd109ac"
+    ));
+    assert!(lockfile.contains(
+        "dotos.git?rev=80c7b17f7ad3cf547d2624c6a243e5de5f85c9f3#80c7b17f7ad3cf547d2624c6a243e5de5f85c9f3"
+    ));
+    assert!(!manifest.contains("branch ="));
+    assert!(!lockfile.contains("?branch="));
     assert!(!manifest.contains("signal-frame.git\", path"));
 }
 
